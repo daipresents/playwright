69b353c9a76da511c8ec3579135d425e725e6e7e
Dai Fujihara <daipresents@gmail.com>
2024-05-20 16:08:45 +0900

    add reg-suit setting and sample test.

---
+++ tests/reg-suit.spec.ts
@@ -0,0 +1,11 @@
+import { test } from '@playwright/test';
+
+test('take screenshots for reg-suit', async ({ page }, testInfo) => {
+  // Base URLを開く
+  await page.goto('https://www.amazon.co.jp/ref=nav_logo', {waitUntil: 'domcontentloaded' });
+
+  // screenshot
+  await page.screenshot({ path: './actual_images/amazon-top.png' });
+});
+
+

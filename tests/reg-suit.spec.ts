59e90a715e431b1f07b4da31c166fad06d8b6641
Dai Fujihara <daipresents@gmail.com>
2024-05-20 16:13:41 +0900

    remove url parameter.

--- tests/reg-suit.spec.ts
+++ tests/reg-suit.spec.ts
@@ -2,7 +2,7 @@ import { test } from '@playwright/test';
 
 test('take screenshots for reg-suit', async ({ page }, testInfo) => {
   // Base URLを開く
-  await page.goto('https://www.amazon.co.jp/ref=nav_logo', {waitUntil: 'domcontentloaded' });
+  await page.goto('https://www.amazon.co.jp/', {waitUntil: 'domcontentloaded' });
 
   // screenshot
   await page.screenshot({ path: './actual_images/amazon-top.png' });
